b99aa2778fdc8c8a46b341c1f089ea13359e892c
Aleksandr Davydochkin <f1reb411111@gmail.com>
2022-02-13 00:37:36 +0300

    Setup media files serving

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -119,10 +119,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
