003b88e9ee3f8398880b7d3618c224d611dbdef4
Aleksandr Davydochkin <30906817+f1reb411@users.noreply.github.com>
2022-02-13 21:24:21 +0300

    Delete environments

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = 'asdsasadsadsfgegegggeg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = False
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': env.str('DB_ENGINE'),
-        'NAME': os.path.join(BASE_DIR / env.str('DB_NAME')),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
